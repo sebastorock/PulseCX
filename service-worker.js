04d09e036194d4de697124355f71b738071516b6
Diego Rog <dsrog@teco.com.ar>
2026-08-26 22:35:49 -0300

    Fix quality preload runtime assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pulsecx-ucema-v7";
+const CACHE_NAME = "pulsecx-ucema-v8";
 const APP_FILES = [
   "./",
   "./index.html",
@@ -6,6 +6,8 @@ const APP_FILES = [
   "./vendor/face-api.min.js",
   "./vendor/ort.min.js",
   "./vendor/ort-wasm-simd-threaded.wasm",
+  "./vendor/ort-wasm-simd-threaded.jsep.mjs",
+  "./vendor/ort-wasm-simd-threaded.jsep.wasm",
   "./models/tiny_face_detector_model-weights_manifest.json",
   "./models/tiny_face_detector_model-shard1",
   "./models/age_gender_model-weights_manifest.json",
